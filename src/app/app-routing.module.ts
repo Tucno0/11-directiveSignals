7f9e054d21c67c5b5c6534ef36937bb48b6f10b8
Jhampier Tucno <yantucnoperez@gmail.com>
2023-06-02 11:06:00 -0500

    Seccion 23: Directivas personalizadas de Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products', // localhost:4200/products
+    loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'products'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/directives/custom-label.directive.ts
@@ -0,0 +1,67 @@
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
+
+@Directive({
+  selector: '[customLabel]'
+})
+export class CustomLabelDirective implements OnInit {
+
+  private htmlElement?: ElementRef<HTMLElement>;
+  private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
+
+  @Input() set color( value: string ) {
+    this._color = value;
+    this.setStyle();
+  }
+
+  @Input() set errors( value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    console.log('Errors: ', this._errors);
+    this.setErrorMessages();
+  }
+
+  constructor( private el: ElementRef<HTMLElement>) {
+    console.log('Constructor de la directiva custom-label');
+    console.log(el);
+    this.htmlElement = el;
+
+    // this.htmlElement.nativeElement.innerHTML = 'Hola mundo desde la directiva custom-label'
+  }
+
+  ngOnInit(): void {
+    console.log('OnInit de la directiva custom-label');
+
+    this.setStyle();
+  }
+
+  setStyle() {
+    if( !this.htmlElement )  return;
+    this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessages() {
+    if ( !this.htmlElement ) return;
+    if ( !this._errors ) {
+      this.htmlElement.nativeElement.innerText = 'No hay errores';
+      return;
+    };
+
+    const errors = Object.keys(this._errors);
+    if (errors.includes('required')) {
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido';
+      return;
+    }
+    if (errors.includes('minlength')) {
+      const min = this._errors['minlength'].requiredLength;
+      const current = this._errors['minlength'].actualLength;
+      this.htmlElement.nativeElement.innerText = `Este campo debe tener al menos ${min} caracteres y tiene ${current}`;
+      return;
+    }
+    if (errors.includes('email')) {
+      this.htmlElement.nativeElement.innerText = 'Este campo debe ser un email válido';
+      return;
+    }
+  }
+
+}
